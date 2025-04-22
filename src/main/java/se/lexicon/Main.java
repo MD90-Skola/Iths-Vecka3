2cb720f85f8b25a374364b1bb2fd077b706ae950
MD90-Skola <micheldahl90@outlook.com>
2025-04-22 18:38:36 +0200

    lektion 2

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,32 +1,14 @@
 package se.lexicon;
 
-// Vecka 3 - Lektion 1 -  https://www.youtube.com/watch?v=vJxaoGQ1dnQ
+// Vecka 3 - Lektion 2 -  https://youtu.be/CiJBFWYO8sg
 
-// vad är ett block?
-// varför indentering?
-// få mer förståelse
+// Array
+// Vad är det ?
+// när är det bra att använda?
 
 
 public class Main {
     public static void main(String[] args) {
-
-        int nr1 = 10;
-        int nr2 = 15;
-
-        if (nr1 > nr2){
-            int nr3 = nr1 + nr2;
-
-        }
-        // vi kommer inte åt vår INT nr 3 eftersom den inte existerar utanför vår if sats och dess kodblock
-        int nr4 = nr3;
-
-
-        if(nr1 != nr2){
-            System.out.println("hej");
-        }
-
-
-
 
 
 
