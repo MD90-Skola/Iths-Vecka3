8310a21cf17440d4e21343560c28f448020ecf1e
MD90-Skola <micheldahl90@outlook.com>
2025-04-22 18:54:55 +0200

    lektion 2

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,4 +1,6 @@
 package se.lexicon;
+import java.util.Arrays;
+
 
 // Vecka 3 - Lektion 2 -  https://youtu.be/CiJBFWYO8sg
 
@@ -7,10 +9,31 @@ package se.lexicon;
 // när är det bra att använda?
 
 
+// arrays kan bara inehålla en tip exempel "strin eller int" går ej blanda
+// när man skapar en array så bestämmer man hur många platser som ska finnas ( går ej endra)
+// man kan ändra värdet
+
+
+// skapa en array när jag vet vilka värden som ska sparas
 public class Main {
     public static void main(String[] args) {
 
+        // array börjar alltid på noll 0
+        String[] classList = {"Robin", "Sofie", "Daniel", "Bob", "lotta"};
+        System.out.println(classList[0]);
 
 
+        // skapa en array när jag inte vet vilka värden jag vill spara i min array
+
+        String[] classList2 = new String[3];
+        //   System.out.println(classList[5]);
+
+        // skriv ut alla värden i min array genom att använda for loop
+        for (int i = 0; i < classList.length; i++) {
+            System.out.println(classList[i]);
+        }
+
+
+        System.out.println(Arrays.toString(classList));
     }
 }
