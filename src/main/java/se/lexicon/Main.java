aa93f1504409c01779be3ad806a855ca10bc491c
MD90-Skola <micheldahl90@outlook.com>
2025-04-22 19:29:13 +0200

    lektion 3 metoder hoppa

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,39 +1,52 @@
 package se.lexicon;
-import java.util.Arrays;
+
+import java.util.Scanner;
+
+// Vecka 3 - Lektion 3 -https://youtu.be/7AqccWNxHTs
+
+// Metoder
+
+//återanvända kod
+// dela upp kod
 
 
-// Vecka 3 - Lektion 2 -  https://youtu.be/CiJBFWYO8sg
-
-// Array
-// Vad är det ?
-// när är det bra att använda?
-
-
-// arrays kan bara inehålla en tip exempel "strin eller int" går ej blanda
-// när man skapar en array så bestämmer man hur många platser som ska finnas ( går ej endra)
-// man kan ändra värdet
-
-
-// skapa en array när jag vet vilka värden som ska sparas
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        // array börjar alltid på noll 0
-        String[] classList = {"Robin", "Sofie", "Daniel", "Bob", "lotta"};
-        System.out.println(classList[0]);
+        System.out.println("skriv in 2 st, heltal så skriver jag ut summan av dom 2 talen.");
+        int input = sc.nextInt();
+        int input2 = sc.nextInt();
 
+        add(input, input2);
 
-        // skapa en array när jag inte vet vilka värden jag vill spara i min array
-
-        String[] classList2 = new String[3];
-        //   System.out.println(classList[5]);
-
-        // skriv ut alla värden i min array genom att använda for loop
-        for (int i = 0; i < classList.length; i++) {
-            System.out.println(classList[i]);
-        }
+/*
+        String michel = "michel";
+        greeting(michel);
+*/
 
 
-        System.out.println(Arrays.toString(classList));
+        //  System.out.println("hej1");
     }
+
+
+
+/*
+    static void greeting(){
+        System.out.println("tja!");
+    }
+
+*/
+
+
+    static void greeting(String name) {
+        System.out.println("hej " + name);
+        System.out.println("välkommen till programmering ");
+    }
+
+    static void add(int nr, int nr2) {
+        System.out.println(nr + nr2);
+    }
+
+
 }
